ccb1383f49d1a8efafad585d7cc761b17aab1241
Srgusev <56511839+Srgusev@users.noreply.github.com>
2020-11-23 20:18:14 +0300

    v.2 Update

--- Code.py
+++ Code.py
@@ -1,64 +1,22 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
+# Импорт модуля random
 import random
- 
-Big = 'QWERTYUIOPASDFGHJKLZXCVBNM'
-Low = 'qwertyuiopasdfghjklzxcvbnm'
-Num = '1234567890'
-Spe = '!@#$%^&amp;*()'
- 
-BI = False  # Пароль должен содержать символы в верхнем регистре (True - да | False - нет)
-LO = True  # Пароль должен содержать символы в нижнем регистре (True - да | False - нет)
-NU = True  # Пароль должен содержать цифры (True - да | False - нет)
-PS = True  # Пароль должен содержать спец символы (True - да | False - нет)
- 
-Password_len = input('Длина пароля: ')
- 
-if Password_len:
-   if Password_len.isdigit() == True:
-       Password_len = int(Password_len)
-   else:
-      print('Выход... Значение должно быть цифровое...')
-      exit(0)
-else:
-   print('Выход... Не указана Длина пароля...')
-   exit(0)
- 
-Password_cou = input('Количество паролей: ')
-print('\n')
- 
-if Password_cou:
-   if Password_cou.isdigit() == True:
-       Password_cou = int(Password_cou)
-   else:
-      print('Выход... Значение должно быть цифровое...')
-      exit(0)
-else:
-   print('Выход... Не указано нужное количество паролей...')
-   exit(0)
- 
-Pass_Symbol = []
-if BI == True:
-   Pass_Symbol.extend(list(Big))
- 
-if LO == True:
-   Pass_Symbol.extend(list(Low))
- 
-if NU == True:
-   Pass_Symbol.extend(list(Num))
- 
-if PS == True:
-   Pass_Symbol.extend(list(Spe))
- 
-random.shuffle(Pass_Symbol)
-psw = []
- 
-for yx in range(Password_cou):
-   psw.append(''.join([random.choice(Pass_Symbol) for x in range(Password_len)]))
- 
-file_Pass = open('Password.txt', 'w')
-file_Pass.write('\n'.join(psw))
-file_Pass.close()
- 
-print('\n'.join(psw))
-print('\n')
+# Создаем функцию pass_gen
+def pass_gen(length):
+    digits='1234567890'
+    leters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    leters_2='abcdefghijklmnopqrstuvwxyz'
+    symbols='!@#$%^&*()-+'
+    password=''
+    var=[digits,leters,leters_2,symbols]
+# Генерация пароля
+    if length<12:
+        return print('Ошибка! Пароль должен иметь не менее 12 символов')
+    else:
+        password+=random.choice(digits)
+        password+=random.choice(leters)
+        password+=random.choice(leters_2)
+        password+=random.choice(symbols)
+        while len(password)<length:
+            password+=random.choice(var[random.randint(0,3)])
+        print(password)
+pass_gen(12)
